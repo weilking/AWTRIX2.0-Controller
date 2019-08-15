0aa6cc0cfe03b7d8b1989943bd6a3213daa39769
Mazze2000 <Mazze2000@web.de>
2019-08-15 21:32:43 +0200

    Update AWTRIXController.cpp

--- src/AWTRIXController.cpp
+++ src/AWTRIXController.cpp
@@ -1,5 +1,34 @@
 //This is only a test...
 
+#include <FS.h>
+#include <spi.h>
+//#include <ArduinoOTA.h>           //https://lastminuteengineers.com/esp32-ota-updates-arduino-ide/
+#include <WiFI.h>                   //neu -> ersetzt ESP8266WiFi.h
+//#include <ESP8266WiFi.h>
+#include <WebServer.h>              //neu -> ersetzt ESP8266WebServer.h
+//#include <ESP8266WebServer.h>
+//#include <ESP32httpUpdate>        //neu -> ersetzt ESP8266httpUpdate.h  (muss noch installiert werden...)
+//#include <ESP8266httpUpdate.h>
+
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <FastLED.h>
+#include <FastLED_NeoMatrix.h>
+#include <Fonts/TomThumb.h>
+#include <LightDependentResistor.h>
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+//#include "SoftwareSerial.h"
+#include <DFPlayerMini_Fast.h>
+#include <WiFiManager.h>
+//#include <DoubleResetDetect.h>
+#include <Wire.h>
+#include <BME280_t.h>
+#include "Adafruit_HTU21DF.h"
+
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "Arduino.h"
